f50bd89e47e87f10cd4f57ebca5723cb416ce93d
Soran Alatas <soran.alatas@protonmail.com>
2022-09-17 21:39:30 +0200

    added forms

--- remoteforecaster/with-tailwindcss-app/pages/index.tsx
+++ remoteforecaster/with-tailwindcss-app/pages/index.tsx
@@ -21,6 +21,7 @@ function ApiCall () {
 const Home: NextPage = () => {
   ApiCall()
   return (
+    
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
@@ -29,7 +30,43 @@ const Home: NextPage = () => {
       <h2 className="text-6xl font-bold">
           PM2.5
         </h2>
-        
+        <div className="w-full max-w-xs">
+        <form className="w-full max-w-lg">
+  <div className="flex flex-wrap -mx-3 mb-6">
+  </div>
+  <div className="flex flex-wrap -mx-3 mb-6">
+    <div className="w-full px-3">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+        Distance to work
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="number" placeholder="123456789km"/>
+      <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+    </div>
+  </div>
+  <div className="flex flex-wrap -mx-3 mb-2">
+    <div className="w-full px-3">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+        Method of transportation
+      </label>
+      <div className="relative">
+        <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>Foot or Bike</option>
+          <option>Public transporation</option>
+          <option>Private vehicle</option>
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div>
+    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      </div>
+  </div>
+</form>
+  <p className="text-center text-gray-500 text-xs">
+    &copy;2020 Acme Corp. All rights reserved.
+  </p>
+</div>
     
         <Chart width={400} height={500}/> 
     <Line width={400} height={500}/>
