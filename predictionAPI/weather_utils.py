509c22fa7447723b382c5f693576afd4376bfe17
Simone Porreca <porrecasimone@gmail.com>
2022-09-17 21:19:43 +0200

    Get People Average

--- predictionAPI/weather_utils.py
+++ predictionAPI/weather_utils.py
@@ -1,7 +1,52 @@
 import requests
 from PIL import Image
 from io import BytesIO
+import pandas as pd
 
+
+class People:
+
+    def __init__(self,
+                 data_path: str = './../data/20220915_HackZurich_Sensirion_v1.csv'):
+
+        # Read data
+        self.data = pd.read_csv(data_path,
+                                parse_dates=['Unnamed: 0'])
+
+        # Rename column
+        self.data.rename(columns={'Unnamed: 0': 'Datum',
+                                  'consumed_meals': 'people'},
+                         inplace=True)
+
+        # Compute datetime information
+        self.data['datum_month'] = self.data['datum'].dt.month
+        self.data['datum_dayofweek'] = self.data['datum'].dt.dayofweek
+
+    def get_average_people(self,
+                           dayofweek: int,
+                           month: int):
+
+        """
+        Return the average of the people in the office for the last month over the passed day of the week
+
+        Parameters:
+            dayofweek: Integer day of the week (e.g., Monday = 0)
+            month: Integer current month
+
+        Return:
+            average_people: Integer average number of people over the last month for the specified day of the week
+        """
+
+        # Filter data by month
+        data_month_filtered = self.data[self.data['datum_month'] == month - 1]
+
+        # Filter data by week of day
+        data_dayof_week_filtered = data_month_filtered[data_month_filtered['datum_dayofweek'] == dayofweek]
+
+        # Compute average people
+        average_people = (data_dayof_week_filtered['people'].sum()/len(data_dayof_week_filtered))
+
+        return average_people
 
 class WeatherData:
     def __init__(self, city_id: int = 2657896):
